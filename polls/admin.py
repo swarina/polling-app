41670cdf5591a2a836031707edde5edb7ebd6314
swarina <swarinajaiswal@gmail.com>
2021-01-20 17:41:42 +0530

    Added models

---
+++ polls/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Question,Choice
+
+admin.site.site_header = "Poll App"
+admin.site.site_title = "Poll App Administration"
+admin.site.index_title = "Welcome to the Admin Area"
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+    ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']}),]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question,QuestionAdmin)
+
