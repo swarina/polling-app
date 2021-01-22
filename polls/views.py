92bcad3a5d72f546159c5b468a2960350fd28ea9
swarina <swarinajaiswal@gmail.com>
2021-01-22 23:58:09 +0530

    Views added

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Question, Choice
 
-# Create your views here.
+def index(request):
+    question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'question_list': question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist!")    
+    return render(request, 'polls/detail.html', {'question': question })
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question })
