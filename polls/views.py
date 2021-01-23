cfbb19201ae294eee453b8d32f6bf03f8e17d7e9
swarina <swarinajaiswal@gmail.com>
2021-01-23 23:34:10 +0530

    Added Templates

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from django.urls import reverse
 from .models import Question, Choice
 
 def index(request):
@@ -16,3 +19,19 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question })
+
+def vote(request, question_id):
+    # print(request.POST['choice'])
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votesgit += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
